e540df43c661f69ed5396c540ee427d8a95d1d21
Edney Ferreira Silva <edneysilva20@hotmail.com>
2020-08-20 10:07:31 -0300

    Utilizando Bootstrap para melhorar o visual

--- JS/main.js
+++ JS/main.js
@@ -7,16 +7,20 @@ function consultaCep(){
     
     
     $.ajax({
-        url: "",
+        url: url,
         type: "GET",
         success: function(response){
             console.log(response);
+            //1ª forma
             $("#cep").html(response.cep);
             $("#logradouro").html(response.logradouro);
+            $("#complemento").html(response.complemento);
             $("#bairro").html(response.logradobairrouro);
             $("#localidade").html(response.localidade);
             $("#uf").html(response.uf);
-            $("#ibge").html(response.ibge);
+            $("#titulo_cep").html("CEP" + response.cep)
+            //$("#ibge").html(response.ibge);
+            //2ª forma
             //document.getElementById("cep").innerHTML = response.cep;
             //document.getElementById("logradouro").innerHTML = response.logradouro;
             //document.getElementById("bairro").innerHTML = response.bairro;
