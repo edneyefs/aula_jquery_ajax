c074760cd8305c9a7d04e93adc4a0855c2db7fbe
Edney Ferreira Silva <edneysilva20@hotmail.com>
2020-08-20 01:19:19 -0300

    Buscador de CEP v1.0

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,28 @@
-alert("funcionou");
+
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    
+    console.log(url);
+    
+    
+    $.ajax({
+        url: "",
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#cep").html(response.cep);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.logradobairrouro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#ibge").html(response.ibge);
+            //document.getElementById("cep").innerHTML = response.cep;
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
+            //document.getElementById("ibge").innerHTML = response.ibge;
+        }
+    })
+}
